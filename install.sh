1c7b030eb20567a2b916883b7bd208f5622fd14e
Kyung Yeol Kim <kim.kyungyeol@croquis.com>
2021-05-25 18:09:52 +0900

    add enter bind.

--- install.sh
+++ install.sh
@@ -9,11 +9,10 @@ gh alias set --shell prs \
     | fzf --header "C-v: preview, C-t: tig, C-w: wttw, C-e: approve" \
         --bind "ctrl-t:execute(tig {4})" \
         --bind "ctrl-w:execute(wttw n {3} --base-ref {4})" \
-        --bind "ctrl-e:execute(gh pr review {1} --approve)" \
+        --bind "ctrl-e:execute-silent(gh pr review {1} --approve)" \
         --bind "ctrl-v:toggle-preview" \
+        --bind "enter:execute(gh pr view -c {1})+abort" \
         --preview "CLICOLOR_FORCE=1 gh pr view {1}" \
         --preview-window hidden \
         --border none \
-        --with-nth 1..3,5,6.. \
-    | awk "{print $1}" \
-    | { read NUM; [ -n "$NUM" ] && gh pr view -c $NUM; }'
+        --with-nth 1..3,5,6..'
